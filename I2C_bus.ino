2478ae3e2d30ec4fb0ba2784075b0045a4f614ca
saipraveenbehara <58806446+saipraveenbehara@users.noreply.github.com>
2023-09-12 21:22:50 +0530

    Add files via upload

---
+++ I2C_bus.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <LiquidCrystal_I2C.h> 
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // Display static text
+  display.println("You can ");
+  display.println("see this is ");
+  display.println("working!");
+  display.display();
+  //lcd
+  lcd.begin(16,2);
+  lcd.init();
+  lcd.backlight(); // Enable or Turn On the backlight 
+}
+
+void loop() {
+   // Scroll in various directions, pausing in-between:
+  lcd.setCursor(4,0);
+  lcd.print("Voila!!!");
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+}
